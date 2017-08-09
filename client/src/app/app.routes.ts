65d7265d3dca4dd6f1b3e4ddb910fe60e836b2b4
Fernando Palma <ferpalma21@github.com>
2017-08-09 19:48:28 +0200

    redirect working

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -12,6 +12,7 @@ import { DetailProductComponent } from './detail-product/detail-product.componen
 import { EditProductComponent } from './edit-product/edit-product.component';
 import { OrderListComponent } from './order-list/order-list.component';
 import { DetailOrderComponent } from './detail-order/detail-order.component';
+import { SuccesfullyBoughtComponent } from './succesfully-bought/succesfully-bought.component'
 
 export const routes: Routes = [
   { path: '', component: IndexComponent },
@@ -20,6 +21,7 @@ export const routes: Routes = [
   { path: 'edit', component: EditComponent },
   { path: 'home', component: HomeComponent },
   { path: 'order', component: OrderComponent },
+  { path: 'success', component: SuccesfullyBoughtComponent},
   { path: 'shopdetails', component: ShopDetailComponent },
   { path: 'product', component: ProductListComponent },
   { path: 'new', component: NewProductComponent },
--- server/api/GiftBox/GiftBoxController.js
+++ server/api/GiftBox/GiftBoxController.js
@@ -6,14 +6,14 @@ const ProductModel = require('../Product/ProductModel');
 module.exports = {
 
   list: function(req, res) {
-    GiftBoxModel.find().then( GiftBoxs => {
+    GiftBoxModel.find().then(GiftBoxs => {
       res.json(GiftBoxs);
-    }).catch( err =>{
-        res.status(500).json({
-          message: 'Error when getting GiftBox.',
-          error: err
-        });
+    }).catch(err => {
+      res.status(500).json({
+        message: 'Error when getting GiftBox.',
+        error: err
       });
+    });
   },
 
 
@@ -40,7 +40,9 @@ module.exports = {
 
   create: (req, res) => {
     console.log('imprimiendo req.body.userId => ' + req.body.userId);
-    const {userId} = req.body;
+    const {
+      userId
+    } = req.body;
     console.log("UserID => " + userId);
     User.findOne({
       _id: userId
@@ -49,7 +51,7 @@ module.exports = {
         interest,
         gender
       } = user;
-      if(user.gender && user.interest){
+      if (user.gender && user.interest) {
         ProductModel.find({
           gender: gender,
           category: {
@@ -74,42 +76,24 @@ module.exports = {
             console.log(randomProductMediumId);
             ProductModel.find({
               gender: gender,
-              category: {
-                $in: interest
-              },
+              category: { $in: interest },
               priceCategory: ['high']
             }, (err, products) => {
               const randomProductHigh = products[Math.floor(Math.random() * products.length)];
               console.log(randomProductHigh);
               const randomProductHighId = randomProductHigh._id;
               console.log(randomProductHighId);
-              var giftBox = new GiftBoxModel({
+              let giftBox = new GiftBoxModel({
                 userId: req.body.userId,
                 address: req.body.address,
                 recieve: req.body.recieve,
                 delivery: req.body.delivery,
                 productsID: [randomProductLowId, randomProductMediumId, randomProductHighId]
               });
-              giftBox.save((err, GiftBox) =>{
-                if (err) {
-                  return res.status(500).json({
-                    message: 'Error when creating GiftBox',
-                    error: err
-                  });
-                }
+              giftBox.save()
+                .then(res => res.json())
+                .catch(err => res.json(err));
 
-              });
-              if (err) {
-                return res.status(500).json({
-                  message: 'Error when getting Product by category.',
-                  error: err
-                });
-              }
-              if (!products.length) {
-                return res.status(404).json({
-                  message: 'No such Product'
-                });
-              }
             });
           });
         });
@@ -136,10 +120,10 @@ module.exports = {
         });
       }
 
-      // GiftBox.productID = req.body.productID ? req.body.productID : GiftBox.productID;
+      GiftBox.productID = req.body.productID ? req.body.productID : GiftBox.productID;
 
 
-      GiftBox.save((err, GiftBox)=> {
+      GiftBox.save((err, GiftBox) => {
         if (err) {
           return res.status(500).json({
             message: 'Error when updating GiftBox.',
